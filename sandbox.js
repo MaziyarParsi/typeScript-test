cc87086e5e1207ee601a1549ab2fe734a8293729
maziyar parsi <mparsi1992@gmail.com>
2021-12-01 22:07:18 +0330

    ts-compiler

---
+++ sandbox.js
@@ -0,0 +1,6 @@
+var test = 'mario';
+var test2 = document.querySelectorAll('input');
+console.log(test);
+test2.forEach(function (input) {
+    console.log(input);
+});
---
+++ sandbox.ts
@@ -0,0 +1,7 @@
+const test = 'mario'
+const test2 = document.querySelectorAll('input')
+console.log(test)
+
+test2.forEach(input=>{
+  console.log(input)
+})
